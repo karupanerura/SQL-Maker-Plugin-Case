8d2ec8819fa42913d5d08d215b39f61e78473e2d
karupanerura <karupa@cpan.org>
2013-05-01 22:06:55 +0900

    initialize commit.

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+SQL::Maker::Plugin::Case - It's new $module
+
+# SYNOPSIS
+
+    use SQL::Maker::Plugin::Case;
+
+# DESCRIPTION
+
+SQL::Maker::Plugin::Case is ...
+
+# LICENSE
+
+Copyright (C) karupanerura.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+karupanerura <karupa@cpan.org>
---
+++ lib/SQL/Maker/Plugin/Case.pm
@@ -0,0 +1,39 @@
+package SQL::Maker::Plugin::Case;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+SQL::Maker::Plugin::Case - It's new $module
+
+=head1 SYNOPSIS
+
+    use SQL::Maker::Plugin::Case;
+
+=head1 DESCRIPTION
+
+SQL::Maker::Plugin::Case is ...
+
+=head1 LICENSE
+
+Copyright (C) karupanerura.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+karupanerura E<lt>karupa@cpan.orgE<gt>
+
+=cut
+
